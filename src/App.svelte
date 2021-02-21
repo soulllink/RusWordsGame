388ed512e06a51de9686278549a237e6e3985e8b
soulllink <soullinkslsl@yandex.ru>
2021-02-21 18:29:43 +0600

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,116 @@
+<script>
+  export let name;
+  import Game from "./Game.svelte";
+
+
+  let stypetip = { show: false };
+
+	function toggle() {
+		stypetip.show = !stypetip.show;
+	}
+</script>
+
+<style>
+  
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
+
+  h1 {
+    color: #ff0000;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
+
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
+  .mdl-grid.center-items {
+  justify-content: center;
+  }
+
+</style>
+
+<svelte:head>
+  <link rel="stylesheet" href="./material.min.css">
+  <script src="./material.min.js"></script>
+  <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+</svelte:head>
+
+<main>
+
+  <h1>Части речи</h1>
+  
+
+  {#if stypetip.show}
+  <button class="mdl-button mdl-js-button mdl-button--fab mdl-button--mini-fab" on:click={toggle}>
+    <i class="material-icons">clear</i>
+  </button>
+  <div class="mdl-grid center-items">
+
+    <table class="mdl-data-table mdl-js-data-table mdl-shadow--2dp">
+    <tbody>
+    <tr>
+      <td class="mdl-data-table__cell--non-numeric">Кто? Что?</td>
+      <td>существительное или местоимение</td>
+      
+    </tr>
+    <tr>
+      <td class="mdl-data-table__cell--non-numeric">Какой? Какая? Какое? Какие?</td>
+      <td>прилагательное</td>
+      
+    </tr>
+    <tr>
+      <td class="mdl-data-table__cell--non-numeric">Что делать? Что сделать? Что делает(ют)?</td>
+      <td>глагол</td>
+      
+    </tr>
+    <tr>
+      <td class="mdl-data-table__cell--non-numeric">Как? Куда? Когда? Почему? Зачем?</td>
+      <td>наречие</td>
+      
+    </tr>
+    <tr>
+      <td class="mdl-data-table__cell--non-numeric">Какой? Что делающий? Что сделавший?</td>
+      <td>причастие</td>
+      
+    </tr>
+    <tr>
+      <td class="mdl-data-table__cell--non-numeric">Как? Что делая? Что сделав?</td>
+      <td>деепричастие</td>
+      
+    </tr>
+    
+    </tbody>
+    </table>
+  </div>
+  
+{/if}
+
+{#if !stypetip.show}
+	<button class="mdl-button mdl-js-button mdl-button--fab mdl-button--mini-fab" on:click={toggle}>
+		<i class="material-icons">add</i>
+	</button>
+{/if}
+
+
+<div class="mdl-grid">
+    <div class="mdl-layout-spacer"></div>
+    <div class="mdl-cell mdl-cell--4-col">
+    
+    
+    <Game/>
+    
+    
+    </div>
+    <div class="mdl-layout-spacer"></div>
+</div>
+  
+ 
+</main>
---
+++ src/Game.svelte
@@ -0,0 +1,101 @@
+<script>
+import { onMount } from "svelte";
+
+let data = [];
+let nnumber;
+let wdata = "Привет!"
+let answer = { state: false };
+let y = Math.floor(Math.random() * 12126);
+let liststype = ["Существительное",
+"Прилагательное",
+"Числительное",
+"Местоимение",
+"Глагол",
+"Наречие",
+"Предлог",
+"Союз",
+"Частица",
+"Междометие"];
+let rightwrong = {right: 0, wrong: 0};
+let alertinfo;
+
+
+onMount(async () => {
+        const res = await fetch('./ruswords.json');
+        data = await res.json();
+});
+    
+
+async function handleClick() {
+	let word = data[y]
+	
+    $: wdata = await word['words'];
+    if(nnumber == word['stype']) { 
+		answer.state = true;
+        //alert("Правильно!");
+        y = Math.floor(Math.random() * 12126);
+        $: wdata = await data[y]['words'];
+		rightwrong.right++
+    } else {
+		answer.state = false;
+        //alert("Правильно: " + liststype[word['stype']])
+		$: alertinfo = liststype[word['stype']]
+		rightwrong.wrong++		
+    }
+}  
+
+function clg() {
+	console.log(nnumber);
+}
+    
+</script>
+
+<div class="material-icons mdl-badge mdl-badge--overlap" data-badge="{rightwrong.right}">done</div>
+<div class="material-icons mdl-badge mdl-badge--overlap" data-badge="{rightwrong.wrong}">delete</div>
+
+
+{#await wdata}
+	<!-- promise is pending -->
+	<p>waiting...</p>
+{:then value}
+
+	<!-- promise was fulfilled -->
+	<h2>{value}</h2>
+
+{/await}
+
+{#if answer.state}
+	<span class="mdl-chip">
+    	<span class="mdl-chip__text">Правильно</span>
+	</span>
+{/if}
+{#if !answer.state}
+	<span class="mdl-chip">
+    	<span class="mdl-chip__text">Неправильно</span>
+	</span>
+	{#if alertinfo != undefined}
+		<span class="mdl-chip mdl-chip--contact">
+			<span class="mdl-chip__contact mdl-color--teal mdl-color-text--white">О</span>
+			<span class="mdl-chip__text">{alertinfo}</span>
+		</span>
+	{/if}
+{/if}
+
+<div style="display:grid">
+<button class="mdl-button mdl-js-button" on:click={handleClick} on:click={() => {nnumber = 0}}>Существительное</button>
+<button class="mdl-button mdl-js-button" on:click={handleClick} on:click={() => {nnumber = 1}}>Прилагательное</button>
+<button class="mdl-button mdl-js-button" on:click={handleClick} on:click={() => {nnumber = 2}}>Числительное</button>
+<button class="mdl-button mdl-js-button" on:click={handleClick} on:click={() => {nnumber = 3}}>Местоимение</button>
+<button class="mdl-button mdl-js-button" on:click={handleClick} on:click={() => {nnumber = 4}}>Глагол</button>
+<button class="mdl-button mdl-js-button" on:click={handleClick} on:click={() => {nnumber = 5}}>Наречие</button>
+<button class="mdl-button mdl-js-button" on:click={handleClick} on:click={() => {nnumber = 6}}>Предлог</button>
+<button class="mdl-button mdl-js-button" on:click={handleClick} on:click={() => {nnumber = 7}}>Союз</button>
+<button class="mdl-button mdl-js-button" on:click={handleClick} on:click={() => {nnumber = 8}}>Частица</button>
+<button class="mdl-button mdl-js-button" on:click={handleClick} on:click={() => {nnumber = 9}}>Междометие</button>
+<button class="mdl-button mdl-js-button" on:click={handleClick} on:click={() => {nnumber = -1}}>-1</button>
+<button class="mdl-button mdl-js-button" on:click={handleClick} on:click={() => {nnumber = 404}}>404</button>
+<button class="mdl-button mdl-js-button" on:click={clg}>CLGDEBUG</button>
+</div>
+
+
+
